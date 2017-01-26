6a680478e26f1f5e58de0870293c034d5adf0292
Jaemin <jaemin0216@gmail.com>
2017-01-26 16:45:10 +0900

    Initial commit

---
+++ MemeMe/MemeMe/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by LimJaemin on 2017. 1. 26..
+//  Copyright © 2017년 LimJaemin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func pickAnImage(_ sender: Any) {
+        
+        
+    }
+
+}
+
