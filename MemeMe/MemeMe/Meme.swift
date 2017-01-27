90cac854898bdc7b61e0eb41ee8ec8c04ca3b92f
Jaemin <jaemin0216@gmail.com>
2017-01-27 19:54:05 +0900

    Add Meme.swift

---
+++ MemeMe/MemeMe/Meme.swift
@@ -0,0 +1,16 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by LimJaemin on 2017. 1. 27..
+//  Copyright © 2017년 LimJaemin. All rights reserved.
+//
+
+import UIKit
+
+struct Meme {
+    var textFielTop: String!
+    var textFieldBottom: String!
+    var originalImage: UIImage!
+    var memedImage: UIImage!
+}
