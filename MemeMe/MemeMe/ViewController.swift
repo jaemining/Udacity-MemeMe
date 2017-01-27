56f3b9e120c6d9530cd514c45c0be2fd45e2fe6d
Jaemin <jaemin0216@gmail.com>
2017-01-27 13:14:03 +0900

    Make pickAnImage() function

--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -14,17 +14,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func pickAnImage(_ sender: Any) {
+        let pickerController = UIImagePickerController()
         
+        pickerController.sourceType = .photoLibrary
+        pickerController.allowsEditing = true
         
+        present(pickerController, animated: true, completion: nil)
     }
 
 }
