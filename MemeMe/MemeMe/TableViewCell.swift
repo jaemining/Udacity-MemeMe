c55f65631ef0720cc5a7d83105372626e6c629e9
Jaemin <jaemin0216@gmail.com>
2017-01-28 15:45:31 +0900

    Make TableViewCell.swift

---
+++ MemeMe/MemeMe/TableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewCell.swift
+//  MemeMe
+//
+//  Created by LimJaemin on 2017. 1. 28..
+//  Copyright © 2017년 LimJaemin. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var tableImageView: UIImageView!
+    @IBOutlet weak var tableTopTextField: UITextField!
+    @IBOutlet weak var tableBottomTextField: UITextField!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
