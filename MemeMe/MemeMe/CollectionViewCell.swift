6d5e36b92640404c60ed55c61df487a28380f497
Jaemin <jaemin0216@gmail.com>
2017-01-28 13:17:36 +0900

    Add table view and collection view

---
+++ MemeMe/MemeMe/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  MemeMe
+//
+//  Created by LimJaemin on 2017. 1. 28..
+//  Copyright © 2017년 LimJaemin. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
